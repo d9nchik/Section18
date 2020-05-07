f8665e974ae59cf5822598562cfbfc1bda827ca9
Danylo Halaiko <dan.galaiko@gmail.com>
2020-05-07 13:43:21 +0300

    Exercise 22 has been done. A recursive method that converts a decimal number into a hex number as a string.

--- src/sample/exercise21/DecimalToBinary.java
+++ src/sample/exercise21/DecimalToBinary.java
@@ -1,13 +1,19 @@
 package sample.exercise21;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
     public static void main(String[] args) {
-        System.out.println(dec2Bin(18));
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter decimal value: ");
+        int number = input.nextInt();
+        System.out.println("Binary value " + dec2Bin(number));
     }
 
     public static String dec2Bin(int value) {
-        if (value == 0)
-            return "";
+        if (value == 0 || value == 1)
+            return "" + value;
         return dec2Bin(value / 2) + (char) ('0' + value % 2);
     }
 }
--- src/sample/exercise22/DecimalToHex.java
+++ src/sample/exercise22/DecimalToHex.java
@@ -1,9 +1,19 @@
 package sample.exercise22;
 
+import java.util.Scanner;
+
 public class DecimalToHex {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter decimal value: ");
+        int number = input.nextInt();
+        System.out.println("Hex value " + dec2Hex(number));
+    }
+
     public static String dec2Hex(int value) {
-        if (value == 0)
-            return "";
+        if (value < 8 && value >= 0)
+            return "" + value;
         return dec2Hex(value / 8) + (char) ('0' + value % 8);
     }
 }
